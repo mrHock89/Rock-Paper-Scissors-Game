e1aa650d96328076d214f1e09a05ed5a4457c687
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-02 17:04:54 +0530

    create function for playing the game for a single time

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // for computer choice
-function getComputerChoice(min, max) {
+function getComputerChoice() {
+  const min = 0;
+  const max = 3;
   number = Math.floor(Math.random() * (max - min) + min);
   if (number === 1) {
     return "Rock";
@@ -9,11 +11,11 @@ function getComputerChoice(min, max) {
     return "Scissors";
   }
 }
-const computerChoice = getComputerChoice(0, 3);
-console.log(computerChoice);
+// const computerChoice = getComputerChoice();
+// console.log(computerChoice);
 
 // Human Choice
-function humanChoice() {
+function getHumanChoice() {
   const number = prompt("Enter 1. Rock 2. Paper and 3. Scissors");
   if (number == 1) {
     return "Rock";
@@ -23,8 +25,32 @@ function humanChoice() {
     return "Scissors";
   }
 }
-console.log(humanChoice());
+// console.log(getHumanChoice());
+
+// Function for single play
 const humanScore = 0;
 const computerScore = 0;
 
-function playRound(computerChoice, humanChoice) {}
+function playRound(computerChoosed, humanChoosed) {
+  if (computerChoosed == humanChoosed) {
+    // humanScore = humanScore;
+    // computerScore = computerScore;
+    return `Both choosed same.`;
+  } else if (computerChoosed == "Rock" && humanChoosed == "Scissors") {
+    return `${computerChoosed} beats ${humanChoosed}.`;
+  } else if (computerChoosed == "Sciisor" && humanChoosed == "Paper") {
+    return `${computerChoosed} beats ${humanChoosed}.`;
+  } else if (computerChoosed == "Paper" && humanChoosed == "Rock") {
+    return `${computerChoosed} beats ${humanChoosed}.`;
+  } else if (humanChoosed == "Rock" && computerChoosed == "Scissors") {
+    return `${humanChoosed} beats ${computerChoosed}.`;
+  } else if (humanChoosed == "Sciisor" && computerChoosed == "Paper") {
+    return `${humanChoosed} beats ${computerChoosed}.`;
+  } else if (humanChoosed == "Paper" && computerChoosed == "Rock") {
+    return `${humanChoosed} beats ${computerChoosed}.`;
+  } else return `Wrong Choice.`;
+}
+const computerChoosed = getComputerChoice();
+const humanChoosed = getHumanChoice();
+console.log(computerChoosed, humanChoosed);
+console.log(playRound(computerChoosed, humanChoosed));
