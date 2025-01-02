21a8b98c462469e6b5b6ae8be5035e474250775a
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-02 16:09:56 +0530

    created a function for humanChoice

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // for computer choice
-// function getComputerChoice(min, max) {
-//   number = Math.floor(Math.random() * (max - min) + min);
-//   if (number === 1) {
-//     return "Rock";
-//   } else if (number === 2) {
-//     return "Paper";
-//   } else {
-//     return "Scissors";
-//   }
-// }
-// computerChoice = getComputerChoice(0, 3);
-// console.log(computerChoice);
+function getComputerChoice(min, max) {
+  number = Math.floor(Math.random() * (max - min) + min);
+  if (number === 1) {
+    return "Rock";
+  } else if (number === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+const computerChoice = getComputerChoice(0, 3);
+console.log(computerChoice);
 
 // Human Choice
-humanChoice = prompt("Enter 1. Rock 2. Paper and 3. Scissors");
-if (humanChoice == 1) {
-  humanChoice = "Rock";
-} else if (humanChoice == 2) {
-  humanChoice = "Paper";
-} else {
-  humanChoice = "Scissors";
+function humanChoice() {
+  const number = prompt("Enter 1. Rock 2. Paper and 3. Scissors");
+  if (number == 1) {
+    return "Rock";
+  } else if (number == 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
-console.log(humanChoice);
+console.log(humanChoice());
+const humanScore = 0;
+const computerScore = 0;
+
+function playRound(computerChoice, humanChoice) {}
