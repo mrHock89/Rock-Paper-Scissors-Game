7103067fe5e737b1365f9d5ce3009568cdbccacc
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-02 15:56:27 +0530

    added human choice to make

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 // for computer choice
-function getComputerChoice(min, max) {
-  number = Math.floor(Math.random() * (max - min) + min);
-  if (number === 1) {
-    return "Rock";
-  } else if (number === 2) {
-    return "Paper";
-  } else {
-    return "Scissor";
-  }
+// function getComputerChoice(min, max) {
+//   number = Math.floor(Math.random() * (max - min) + min);
+//   if (number === 1) {
+//     return "Rock";
+//   } else if (number === 2) {
+//     return "Paper";
+//   } else {
+//     return "Scissors";
+//   }
+// }
+// computerChoice = getComputerChoice(0, 3);
+// console.log(computerChoice);
+
+// Human Choice
+humanChoice = prompt("Enter 1. Rock 2. Paper and 3. Scissors");
+if (humanChoice == 1) {
+  humanChoice = "Rock";
+} else if (humanChoice == 2) {
+  humanChoice = "Paper";
+} else {
+  humanChoice = "Scissors";
 }
-computerChoice = getComputerChoice(0, 3);
-console.log(computerChoice);
+console.log(humanChoice);
