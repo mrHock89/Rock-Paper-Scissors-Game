739d74b7de21d172cc805f3807c27da5c4074855
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-10 07:24:28 +0530

    finished the game implementaition

--- script.js
+++ script.js
@@ -74,10 +74,22 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const div = document.createElement("div");
-div.setAttribute("class", "score");
-const RunningSrcore = document.createElement("p");
-RunningSrcore.setAttribute("class", "r_socre");
+// creating all required elements
+const scoreDiv = document.createElement("div");
+scoreDiv.id = "scoreDiv";
+const mainBody = document.body;
+
+const infoScore = document.createElement("p");
+infoScore.setAttribute("id", "infoScore");
+
+const humanScoreBoard = document.createElement("h3");
+humanScoreBoard.setAttribute("id", "humanScoreBoard");
+
+const computerScoreBoard = document.createElement("h3");
+computerScoreBoard.setAttribute("id", "computerScoreBoard");
+
+const winMessage = document.createElement("h2");
+winMessage.setAttribute("id", "winMessage");
 
 // computer choice function
 function computerChoice() {
@@ -91,47 +103,146 @@ function computerChoice() {
 
 // human choice function
 let human_choice;
+let computer_choice;
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     human_choice = button.id;
-    playRound(computerChoice(), human_choice);
+    computer_choice = computerChoice();
+    console.log(
+      `Computer Choice: ${computer_choice} and Human Choice: ${human_choice}`
+    );
+    // Playing The game
+    playGame(computer_choice, human_choice);
   });
 });
 
-function playRound(computerChoosed, humanChoosed) {
-  if (humanChoosed == "Reset") {
-    humanScore = 0;
+function playGame(computerMove, humanMove) {
+  // a function that will update score
+  function updateScoreInScoreBoard(computerScore, humanScore, textInfo) {
+    infoScore.textContent = textInfo;
+    humanScoreBoard.textContent = `Human Score: ${humanScore}`;
+    computerScoreBoard.textContent = `Computer Score: ${computerScore}`;
+  }
+
+  // if Reset button is pressed
+  if (humanMove === "Reset") {
     computerScore = 0;
+    humanScore = 0;
+    updateScoreInScoreBoard(computerScore, humanScore, "Get is Restarted!");
   }
-  if (computerChoosed == humanChoosed) {
-    humanScore = humanScore;
-    computerScore = computerScore;
-  } else if (computerChoosed == "Rock" && humanChoosed == "Scissors") {
-    computerScore++;
-  } else if (computerChoosed == "Scissors" && humanChoosed == "Paper") {
-    computerScore++;
-  } else if (computerChoosed == "Paper" && humanChoosed == "Rock") {
-    computerScore++;
-  } else if (humanChoosed == "Rock" && computerChoosed == "Scissors") {
-    humanScore++;
-  } else if (humanChoosed == "Scissors" && computerChoosed == "Paper") {
-    humanScore++;
-  } else if (humanChoosed == "Paper" && computerChoosed == "Rock") {
-    humanScore++;
+  if (computerMove === humanMove) {
+    updateScoreInScoreBoard(
+      computerScore,
+      humanScore,
+      `computer: ${computerMove} Vs human: ${humanMove}`
+    );
   }
-  Score(computerScore, humanScore);
+  if (computerMove === "Rock" && humanMove === "Scissors") {
+    computerScore++;
+    updateScoreInScoreBoard(
+      computerScore,
+      humanScore,
+      `computer: ${computerMove} Vs human: ${humanMove}`
+    );
+  } else if (computerMove === "Scissors" && humanMove === "Paper") {
+    computerScore++;
+    updateScoreInScoreBoard(
+      computerScore,
+      humanScore,
+      `computer: ${computerMove} Vs human: ${humanMove}`
+    );
+  } else if (computerMove === "Paper" && humanMove === "Rock") {
+    computerScore++;
+    updateScoreInScoreBoard(
+      computerScore,
+      humanScore,
+      `computer: ${computerMove} Vs human: ${humanMove}`
+    );
+  } else if (humanMove === "Rock" && computerMove === "Scissors") {
+    humanScore++;
+    updateScoreInScoreBoard(
+      computerScore,
+      humanScore,
+      `computer: ${computerMove} Vs human: ${humanMove}`
+    );
+  } else if (humanMove === "Scissors" && computerMove === "Paper") {
+    humanScore++;
+    updateScoreInScoreBoard(
+      computerScore,
+      humanScore,
+      `computer: ${computerMove} Vs human: ${humanMove}`
+    );
+  } else if (humanMove === "Paper" && computerMove === "Rock") {
+    humanScore++;
+    updateScoreInScoreBoard(
+      computerScore,
+      humanScore,
+      `computer: ${computerMove} Vs human: ${humanMove}`
+    );
+  }
+
+  scoreBoard(computerScore, humanScore);
 }
+
+function scoreBoard(computerScore, humanScore) {
+  if (computerScore == 5) {
+    winMessage.textContent = "Computer is the winner!";
+    scoreDiv.appendChild(winMessage);
+  } else if (humanScore == 5) {
+    winMessage.textContent = "Human is the winner";
+    scoreDiv.appendChild(winMessage);
+  }
+}
+
+infoScore.style.color = "blue";
+infoScore.style.fontStyle = "italic";
+
+humanScoreBoard.textContent = "Human Score: " + humanScore;
+computerScoreBoard.textContent = "Computer Score: " + computerScore;
+
+scoreDiv.setAttribute(
+  "style",
+  "margin-top: 15px; padding: 5px; border: 3px solid black;"
+);
+
+scoreDiv.appendChild(infoScore);
+scoreDiv.appendChild(humanScoreBoard);
+scoreDiv.appendChild(computerScoreBoard);
+
+// function playRound(computerChoosed, humanChoosed) {
+//   if (humanChoosed == "Reset") {
+//     humanScore = 0;
+//     computerScore = 0;
+//   }
+//   if (computerChoosed == humanChoosed) {
+//     humanScore = humanScore;
+//     computerScore = computerScore;
+//   } else if (computerChoosed == "Rock" && humanChoosed == "Scissors") {
+//     computerScore++;
+//   } else if (computerChoosed == "Scissors" && humanChoosed == "Paper") {
+//     computerScore++;
+//   } else if (computerChoosed == "Paper" && humanChoosed == "Rock") {
+//     computerScore++;
+//   } else if (humanChoosed == "Rock" && computerChoosed == "Scissors") {
+//     humanScore++;
+//   } else if (humanChoosed == "Scissors" && computerChoosed == "Paper") {
+//     humanScore++;
+//   } else if (humanChoosed == "Paper" && computerChoosed == "Rock") {
+//     humanScore++;
+//   }
+//   Score(computerScore, humanScore);
+// }
 
 // Score Board
 
-function Score(computer_score, human_score) {
-  if (computer_score === 5) {
-    console.log("The winner is computer.");
-  } else if (human_score === 5) {
-    console.log("The winner is human.");
-  } else {
-    console.log(`${computer_score} ${human_score}`);
-  }
-}
+// function Score(computer_score, human_score) {
+//   if (computer_score === 5) {
+//     console.log("The winner is computer.");
+//   } else if (human_score === 5) {
+//     console.log("The winner is human.");
+//   } else {
+//     console.log(`${computer_score} ${human_score}`);
+//   }
+// }
